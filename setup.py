26993c458a8ff0e5ca22b25e434dec73e303b161
Maria <kuznetsovamaria1996@gmail.com>
2019-06-04 15:30:01 +0300

    changed name

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
-setup(name='ds_toolkit',
+setup(name='dstoolkit',
       version='0.1',
       description='Collection of code to ease ml and dl.',
-      url='https://github.com/sean-mcclure/datapeek_py',
+      url="https://github.com/Smitharry/ds_toolkit",
       author='Maria Kuznetsova',
       author_email='kuznetsovamaria1996@gmail.com',
       packages=['deep_learning'],
+      install_requires=[
+            'keras',
+            'tensorflow',
+            'numpy'
+      ],
       zip_safe=False)
