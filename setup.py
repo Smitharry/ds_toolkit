a9c1e14400c77299f40213d7b71a55c3ddf5b13b
Maria <kuznetsovamaria1996@gmail.com>
2019-06-04 13:22:31 +0300

    Re-upload module

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ds_toolkit',
+      version='0.1',
+      description='Collection of code to ease ml and dl.',
+      url='https://github.com/sean-mcclure/datapeek_py',
+      author='Maria Kuznetsova',
+      author_email='kuznetsovamaria1996@gmail.com',
+      packages=['deep_learning'],
+      zip_safe=False)
