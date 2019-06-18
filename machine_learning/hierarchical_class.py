b3eec7b88e46ba6af1402561c1781f4baa335a94
Maria <kuznetsovamaria1996@gmail.com>
2019-06-18 18:45:29 +0300

    New commit

--- machine_learning/hierarchical_class.py
+++ machine_learning/hierarchical_class.py
@@ -1,14 +1,28 @@
-from sklearn.model_selection import train_test_split
+
 import numpy as np
 
 
 class HierarchicalClassification:
-    def __init__(self, levels, models):
-        self.levels = levels
+    def __init__(self, models, params=dict()):
         self.models = models
+        if params:
+            self.set_params(params)
+
+    def set_params(self, params):
+        for model_index in params:
+            param = params[model_index]
+            self.models[model_index].set_params(**param)
+
+    def get_params(self, deep=False):
+        params = {}
+        for index, model in enumerate(self.models):
+            params[index] = model.get_params()
+        return params
+
+    def collect_info(self, levels):
+        self.levels = levels
         self.n_classes = [len(np.unique(level)) for level in self.levels.transpose()]
         self.uniques = self.find_class_labels(self.levels)
-        self.predicted_classes = None
         self.path = np.unique(self.levels, axis=0)
 
     def find_class_labels(self, levels):
@@ -17,60 +31,36 @@ class HierarchicalClassification:
             uniques.append(set(np.unique(level)))
         return uniques
 
-    def padd_level(self, level, level_index):
-        # level.shape = (number of observations, number of predicted classes)
-        missed_classes = self.uniques[level_index] - self.predicted_classes[level_index]
-        for class_ in missed_classes:
-            level = self.pad_class(level, class_)
-        return level
-
-    def pad_class(self, level, class_):
-        # level.shape = (number of observations, number of predicted classes)
-        # padd absent class either to end of array or into array.
-
-        if class_ < level.shape[1]:
-            level = np.insert(level, class_, 0, axis=1)
-        else:
-            zeros = np.zeros(level.shape[0]).reshape(1, level.shape[0])
-            level = np.concatenate((level, zeros), axis=1)
-        return level
-
-    def add_missed_classes(self, y_pred):
-        # y_pred.shape = (number of hierarchy levels,
-        # number of observations, number of predicted classes)
-        for index, level in enumerate(y_pred):
-            if level.shape[1] != self.n_classes[index]:
-                y_pred[index] = self.padd_level(level, index)
-        return y_pred
-
     def create_mask(self, levels):
         masks = []
         for index, level in enumerate(levels.transpose()):
+            slice = np.logical_not(np.isin(level, list(self.uniques[index])))
+            level[slice] = 0
             mask = np.eye(self.n_classes[index])[level]
+            mask[slice] = mask[slice] * 0
             masks.append(mask)
         return masks
 
-    def calculate_likelihood(self, y_true, y_pred):
-        level_true_probs = []
-        masks = self.create_mask(y_true)
-        for level, mask in zip(y_pred, masks):
-            level_true_prob = np.multiply(level, mask)
-            level_true_prob = level_true_prob.sum(axis=1)
-            level_true_probs.append(level_true_prob)
-        likelihood = np.array(level_true_probs).sum()
+    def calculate_likelihood(self, x, y_true):
+        probs = self.predict_proba(x)
+        likelihood = np.zeros(len(x))
+        for index in range(len(x)):
+            true_label = y_true[index][-1]
+            if true_label not in probs:
+                likelihood[index] = 0
+            else:
+                likelihood[index] = probs[true_label][index]
+        likelihood = np.sum(likelihood)
         return likelihood
 
     def find_predicted_classes(self, y_train):
         self.predicted_classes = self.find_class_labels(y_train)
 
-    def fit(self, X):
-        x_train, x_val, y_train, y_val = train_test_split(X, self.levels, test_size=0.2)
-        self.find_predicted_classes(y_train)
-        self.val = (x_val, y_val)
-        self.train = (x_train, y_val)
+    def fit(self, X, y_train):
+        self.collect_info(y_train)
         for model, y_true in zip(self.models, y_train.transpose()):
-            model.fit(x_train, y_true)
-            x_train = np.concatenate((x_train, model.predict_proba(x_train)), axis=1)
+            model.fit(X, y_true)
+            X = np.concatenate((X, model.predict_proba(X)), axis=1)
 
     def predict(self, x):
         y_pred = []
@@ -80,18 +70,14 @@ class HierarchicalClassification:
         return y_pred
 
     def predict_proba(self, x):
-        y_pred = []
-        for model in self.models:
-            y_pred.append(model.predict_proba(x))
-            x = np.concatenate((x, model.predict_proba(x)), axis=1)
-        y_pred = self.add_missed_classes(y_pred)
+        y_pred = self.predict(x)
         probs = self.calculate_prob(y_pred, len(x))
         return probs
 
     def calculate_prob(self, y_pred, length):
         probs_list = {}
         for path in self.path:
-            y_true = np.array([np.ones(length, dtype=np.int32)*path[i] for i in range(len(path))]).transpose()
+            y_true = np.array([np.ones(length, dtype=np.int32) * path[i] for i in range(len(path))]).transpose()
             masks = self.create_mask(y_true)
             level_true_probs = []
             for level, mask in zip(y_pred, masks):
@@ -102,8 +88,4 @@ class HierarchicalClassification:
             for level in level_true_probs:
                 probs = probs * np.array(level)
             probs_list[path[-1]] = probs
-        prob_array = []
-        for i in range(self.n_classes[-1]):
-            prob_array.append(probs_list[i])
-        prob_array = np.array(prob_array)
-        return prob_array
+        return probs_list
